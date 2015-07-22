7d3643668a0b3145182f6e4ae7e872a640f761d0
detec <dlit@yandex.ru>
2015-07-22 06:42:46 +0300

    Partially fixed converter issue

--- src/org/openbox/sf5/application/CategoryConverter.java
+++ src/org/openbox/sf5/application/CategoryConverter.java
@@ -1,22 +1,31 @@
 package org.openbox.sf5.application;
 
+import java.io.Serializable;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-import org.openbox.sf5.db.Transponders;
+import org.openbox.sf5.db.Satellites;
 import org.openbox.sf5.service.ObjectsController;
 
 @Named("transpondersConverter")
 @FacesConverter(value = "transpondersConverter")
-public class CategoryConverter implements Converter {
+@ViewScoped
+public class CategoryConverter implements Converter, Serializable {
 
 	// @PersistenceContext(unitName = "luavipuPU")
 	// I include this because you will need to
 	// lookup your entities based on submitted values
 	// private transient EntityManager em;
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 9161349494175348253L;
 
 	@Override
 	public Object getAsObject(FacesContext ctx, UIComponent component,
@@ -26,14 +35,18 @@ public class CategoryConverter implements Converter {
 		// returned from the client side
 		// return em.find(Category.class, new BigInteger(value));
 		ObjectsController contr = new ObjectsController();
-		return contr.select(Transponders.class, Long.parseLong(value));
+		return contr.select(Satellites.class, Long.parseLong(value));
 	}
 
 	@Override
 	public String getAsString(FacesContext fc, UIComponent uic, Object o) {
 		// This will return view-friendly output for the dropdown menu
 		// return ((Category) o).name();
-		return ((Transponders) o).toString();
+		if (o != null) {
+			return ((Satellites) o).toString();
+		} else {
+			return "";
+		}
 
 	}
 }
