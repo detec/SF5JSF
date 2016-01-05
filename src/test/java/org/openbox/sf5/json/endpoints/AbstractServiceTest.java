ea28c87d82c92bd1d49edda002f8a828e8da408b
duplyk.a <duplyk.a@developer.malbi.lan>
2016-01-05 17:12:03 +0200

    Moved to MOXy on client - Satellites And Transponders IT tests OK.

--- src/test/java/org/openbox/sf5/json/endpoints/AbstractServiceTest.java
+++ src/test/java/org/openbox/sf5/json/endpoints/AbstractServiceTest.java
@@ -6,11 +6,11 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.moxy.xml.MoxyXmlFeature;
 import org.openbox.sf5.json.config.JacksonObjectMapperConfiguration;
+import org.openbox.sf5.json.config.MOXyJsonContextResolver;
 import org.openbox.sf5.json.config.MyApplicationResourceConfig;
-import org.openbox.sf5.json.config.MyObjectMapperProvider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -36,35 +36,35 @@ public abstract class AbstractServiceTest {
 
 		configureMapper();
 
-		//JacksonJsonProvider provider = new JacksonJsonProvider(mapper);
+		// JacksonJsonProvider provider = new JacksonJsonProvider(mapper);
 
 		return ClientBuilder.newBuilder()
 
 				// 18.12.2015, will try MOXy, not Jackson
 				// .register(MoxyJsonFeature.class)
 
-				//.register(JacksonJaxbJsonProvider.class)
+				// .register(JacksonJaxbJsonProvider.class)
 
 				// without Jackson feature Strings are not unmarshalled
 				// But MOXy doesn't understand this date format
-				.register(JacksonFeature.class)
-				.register(MyObjectMapperProvider.class)
+				// .register(JacksonFeature.class)
 
-				//.register(JacksonJsonProvider.class)
+				// .register(MyObjectMapperProvider.class)
+
+				// 05.01.2016, trying to use MOXy
+				.register(MoxyXmlFeature.class)
+
+				.register(MOXyJsonContextResolver.class)
+
+				// .register(JacksonJsonProvider.class)
 
 				// registering configured provider with mapper
-				//.register(provider)
-
-				// https://cwiki.apache.org/confluence/display/CXF20DOC/JAX-RS+Data+Bindings
-				// As per JAX-RS 2.0 specification, the support of JSR-353 Java API for JSON Processing is mandatory requirement and implies the presence of message body reader(s)/writer(s) for following types: JsonStructure, JsonArray and JsonObject.
-				// It doesn't help
-//				.register(MessageBodyReader.class)
-//				.register(MessageBodyWriter.class)
+				// .register(provider)
 
 				.register(MultiPartFeature.class)
 
 				// we have the same objectmapper config for client and server.
-				//.register(MarshallingFeature.class)
+				// .register(MarshallingFeature.class)
 
 				// Log showed that transponders come OK.
 				// .register(new LoggingFilter(LOGGER, true))
@@ -78,13 +78,6 @@ public abstract class AbstractServiceTest {
 	}
 
 	public void configureMapper() {
-		// mapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING,
-		// true);
-		// mapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING,
-		// true);
-		// mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
-		// mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
-		// false);
 
 		JacksonObjectMapperConfiguration.configureMapper(mapper);
 	}
