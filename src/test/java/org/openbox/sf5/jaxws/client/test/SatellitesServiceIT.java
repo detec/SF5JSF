3a513afe6ecf114904e12dfe017e8c0758bf07e3
detec <dlit@yandex.ru>
2016-01-13 20:21:51 +0200

    Moved hibernate.cfg,xml to resources folder.

--- src/test/java/org/openbox/sf5/jaxws/client/test/SatellitesServiceIT.java
+++ src/test/java/org/openbox/sf5/jaxws/client/test/SatellitesServiceIT.java
@@ -2,16 +2,12 @@ package org.openbox.sf5.jaxws.client.test;
 
 import java.net.URL;
 
-import javax.xml.namespace.QName;
-
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.junit.runners.MethodSorters;
-import org.openbox.sf5.jaxws.client.gen.Satellites;
-import org.openbox.sf5.jaxws.client.gen.SatellitesService;
 
 // https://github.com/javaee-samples/javaee7-samples/blob/master/jaxws/jaxws-client/src/test/java/org/javaee7/jaxws/client/EBookStoreClientSampleTest.java
 
@@ -19,20 +15,20 @@ import org.openbox.sf5.jaxws.client.gen.SatellitesService;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SatellitesServiceIT {
 
-	private SatellitesService satellitesService;
+	// private SatellitesService satellitesService;
 
 	private URL url;
 
-	private Satellites satPort;
+	// private Satellites satPort;
 
 	@Before
 	public void setUp() throws Exception {
 		url = new URL("http://localhost:8080/SF5JSF-test/");
 
-		satellitesService = new SatellitesService(new URL(url, "SatellitesService?wsdl"),
-				new QName("http://sf5.openbox.org/satelliteservice/1.0", "SatellitesService"));
-
-		satPort = satellitesService.getSatellitesPort();
+//		satellitesService = new SatellitesService(new URL(url, "SatellitesService?wsdl"),
+//				new QName("http://sf5.openbox.org/satelliteservice/1.0", "SatellitesService"));
+//
+//		satPort = satellitesService.getSatellitesPort();
 	}
 
 	@Test
