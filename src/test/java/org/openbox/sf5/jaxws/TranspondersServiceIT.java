a830ca69549472844ece2ec88021c7cbf86940f2
duplyk.a <duplyk.a@developer.malbi.lan>
2016-01-16 12:30:44 +0200

    Added XML validation to ws model class Transponders.

--- src/test/java/org/openbox/sf5/jaxws/TranspondersServiceIT.java
+++ src/test/java/org/openbox/sf5/jaxws/TranspondersServiceIT.java
@@ -1,8 +1,15 @@
 package org.openbox.sf5.jaxws;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -17,9 +24,14 @@ import org.openbox.sf5.wsmodel.WSException_Exception;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TranspondersServiceIT extends AbstractWSTest {
 
+	private Validator validator;
+
 	@Before
 	public void setUp() throws Exception {
 		setUpAbstract();
+
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
 	}
 
 	@Test
@@ -34,6 +46,7 @@ public class TranspondersServiceIT extends AbstractWSTest {
 
 		assertThat(transList).isNotNull();
 		assertThat(transList.size()).isGreaterThan(0);
+		validateTranspondersList(transList);
 	}
 
 	@Test
@@ -58,6 +71,7 @@ public class TranspondersServiceIT extends AbstractWSTest {
 		}
 		assertThat(transList).isNotNull();
 		assertThat(transList.size()).isGreaterThan(0);
+		validateTranspondersList(transList);
 	}
 
 	@Test
@@ -73,6 +87,19 @@ public class TranspondersServiceIT extends AbstractWSTest {
 
 		assertThat(transList).isNotNull();
 		assertThat(transList.size()).isGreaterThan(0);
+		validateTranspondersList(transList);
+	}
+
+	public void validateTranspondersList(List<Transponders> transList) {
+		int[] resultArray = new int[1];
+
+		transList.forEach(t -> {
+			Set<ConstraintViolation<Transponders>> constraintViolations = validator.validate(t);
+			resultArray[0] = resultArray[0] + constraintViolations.size();
+
+		});
+
+		assertEquals(0, resultArray[0]);
 	}
 
 }
